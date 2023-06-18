71a63d7b7b319984347ccf7a96f193c14f0a3f52
Mariam Akiashvili <76559977+MariamAkiashvili@users.noreply.github.com>
2023-06-18 16:47:53 +0400

    Add files via upload

--- DeployInertia/src/main/java/com/example/demoInertia/controller/UploadOrganizationImagesController.java
+++ DeployInertia/src/main/java/com/example/demoInertia/controller/UploadOrganizationImagesController.java
@@ -1,19 +1,22 @@
 package com.example.demoInertia.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.demoInertia.model.Organization;
-import com.example.demoInertia.service.UploadImageService;
+import com.example.demoInertia.service.ImageService;
+
+import jakarta.annotation.Resource;
 
 @RestController
 @RequestMapping("/Upload")
@@ -21,7 +24,7 @@ import com.example.demoInertia.service.UploadImageService;
 public class UploadOrganizationImagesController {
 
     @Autowired 
-    UploadImageService uploadImageService;
+    ImageService imageService;
     
     @PostMapping("/Image")
     public String handleImageUpload( @RequestParam("organizationId") int organizationId, 
@@ -30,9 +33,16 @@ public class UploadOrganizationImagesController {
 
         // int id = Integer.parseInt(StringOrganizationId);
         for (MultipartFile f : file){
-            uploadImageService.uploadFile(f, organizationId);
+            imageService.uploadFile(f, organizationId);
         }
         
         return "Upload completed successfully";
+    }
+
+    @GetMapping("GetImages")
+    public  ResponseEntity<List<Resource>> getImagesById(@RequestParam int id){
+        return null; 
+        
+        
     }
 }
